e63a825bfc93aeb000fb3d2b885d5bf641c4b2ca
stonehell <2499144744@qq.com>
2017-09-20 22:40:33 +0800

    alipay,some problem

---
+++ imooc-mmall/mmallmine/src/main/java/com/mmall/service/IOrderService.java
@@ -0,0 +1,15 @@
+package com.mmall.service;
+
+import com.github.pagehelper.PageInfo;
+import com.mmall.common.ServerResponse;
+
+import java.util.Map;
+
+public interface IOrderService {
+
+    ServerResponse queryOrderPayStatus(Integer userId, Long orderNo);
+
+    public ServerResponse pay(Long orderNo, Integer userId, String path);
+
+    ServerResponse aliCallback(Map<String, String> params);
+}
