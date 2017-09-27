d8c037df22ca6b31ea1733ba6ec127909ad5a5d3
stonehell <2499144744@qq.com>
2017-09-27 11:33:44 +0800

    controller 一些注解使用

--- Spring Boot/imooc-2hours/src/main/java/com/storm/girl/HelloController.java
+++ Spring Boot/imooc-2hours/src/main/java/com/storm/girl/HelloController.java
@@ -2,11 +2,13 @@ package com.storm.girl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+//@RestController
+@Controller
+@ResponseBody
+@RequestMapping("/hello")
 public class HelloController {
 
     //配置文件里的内容注入
@@ -22,8 +24,23 @@ public class HelloController {
     @Autowired
     private GirlProperties girlProperties;
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @RequestMapping(value = {"/hello", "/hi"}, method = RequestMethod.GET)
     public String say() {
+        //Controller配合templates使用
+//        return "index";
+
+        //加上ResponseBody可以使用
         return girlProperties.getCupSize();
+    }
+
+    @RequestMapping(value = "/{id}/say2", method = RequestMethod.GET)
+    public String say2(@PathVariable("id") Integer id) {
+        return "id:" + id;
+    }
+
+//    @RequestMapping(value = "/say3", method = RequestMethod.GET)
+    @GetMapping(value = "/say3")
+    public String say3(@RequestParam(value = "id",required = false,defaultValue = "0") Integer myId) {
+        return "id:" + myId;
     }
 }
