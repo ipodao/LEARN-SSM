716c091d87a5c42f5db10891539920eec4588c66
stonehell <2499144744@qq.com>
2017-09-27 11:05:05 +0800

    properties,多环境配置

--- Spring Boot/imooc-2hours/src/main/java/com/storm/girl/HelloController.java
+++ Spring Boot/imooc-2hours/src/main/java/com/storm/girl/HelloController.java
@@ -1,5 +1,7 @@
 package com.storm.girl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    //配置文件里的内容注入
+    @Value("${cupSize}")
+    private String cupSize;
+
+    @Value("${age}")
+    private Integer age;
+
+    @Value("${content}")
+    private String content;
+
+    @Autowired
+    private GirlProperties girlProperties;
+
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String say() {
-        return "Hello Spring Boot";
+        return girlProperties.getCupSize();
     }
 }
